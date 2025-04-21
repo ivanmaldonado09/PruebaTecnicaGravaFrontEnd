7c3ba6625505dd1e9255de2faabdc610f9fb5684
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-04-20 21:56:10 -0300

    Agrego seccion 2 y sus estilos

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,39 @@ export default function Home() {
     <div>
     <section className="seccion">
       <div className="imagen">
-        <Image src="/pokemon.jpg" width={1000} height={1000} alt="imagen" className="imagen" />
+        <Image src="/pokemonfondo.jpg" width={1920} height={1080} alt="imagen" className="imagen" />
       </div>
       <div className="texto">
         <h2>Pokemon</h2>
         <h3>La mejor web de pokemon</h3>
         <p>En esta página podes encontrar tus pokemon favoritos, ver sus estadísticas, habilidades y mucho más. 
         <br></br>
-        Además podrás ver sus evoluciones y como conseguirlos.
+        Además, podrás ver sus evoluciones y aprender sobre sus tipos.
         </p>
         
       </div>
     </section>
+
+    <section className="seccion-2">
+  <h2 className="titulo">Habilidades</h2>
+
+  <p className="subtitulo">Estas son las principales habilidades de los pokemon</p>
+
+  <div className="cards">
+    <div className="card">
+      <h3 className="card-titulo">Fuego</h3>
+      <p className="card-texto">Las habilidades de fuego son fuertes contra los pokemon tipo planta</p> 
+    </div>
+    <div className="card">
+      <h3 className="card-titulo">Agua</h3>
+      <p className="card-texto">Las habilidades de agua son fuertes contra los pokemon tipo fuego</p>
+    </div>
+    <div className="card">
+      <h3 className="card-titulo">Planta</h3>
+      <p className="card-texto">Las habilidades de planta son fuertes contra los pokemon tipo agua</p>
+    </div>
+  </div>
+</section>
    </div>
 
 
